2a4d656e404d66969ca096d1d2c112fcd30dc8b1
Malachi Phillips <malachi2@illinois.edu>
2021-09-08 16:57:14 -0400

    Hardcode g++ as OCCA_CXX whenever not using Serial mode

--- src/core/platform.cpp
+++ src/core/platform.cpp
@@ -286,8 +286,7 @@ device_t::device_t(setupAide& options, MPI_Comm comm)
   // overwrite compiler settings to ensure
   // compatability of libocca and kernelLauchner 
   if(this->mode() != "Serial") {
-    std::string buf;
-    buf.assign(getenv("NEKRS_CXX"));
+    std::string buf = "g++";
     setenv("OCCA_CXX", buf.c_str(), 1);
     buf.assign(getenv("NEKRS_CXXFLAGS"));
     setenv("OCCA_CXXFLAGS", buf.c_str(), 1);
