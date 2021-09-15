bfb94a09f525f8e140761dbe29061e46f88dc953
Malachi Phillips <malachi2@illinois.edu>
2021-09-15 16:55:56 -0500

    Cleanup SCNRS statements in turbPipe

---
+++ examples/turbPipe/casedata.h
@@ -0,0 +1,8 @@
+      COMMON /SCNRS/ sc_nrs(10)
+      real                sc_nrs
+      real ZLENGTH
+      integer NSLABS
+      integer NELSLAB
+      ZLENGTH = sc_nrs(1)
+      NSLABS = int(sc_nrs(2))
+      NELSLAB = int(sc_nrs(3))
