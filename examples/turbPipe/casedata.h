c645ba4da3c3e607829e4b71709d079743be4684
Malachi Phillips <malachi2@illinois.edu>
2021-09-16 09:13:43 -0500

    Use Fortran style includes for:   - ethier   - lowMach   - gabls   - mv_cyl   - turbPipe

--- examples/turbPipe/casedata.h
+++
@@ -1,8 +0,0 @@
-      COMMON /SCNRS/ sc_nrs(10)
-      real                sc_nrs
-      real ZLENGTH
-      integer NSLABS
-      integer NELSLAB
-      ZLENGTH = sc_nrs(1)
-      NSLABS = int(sc_nrs(2))
-      NELSLAB = int(sc_nrs(3))
