cea313f19c1ad124e60f510a48ae2aff54ca46e4
Malachi Phillips <malachi2@illinois.edu>
2022-01-14 13:32:17 -0600

    Remove dependence on stride in ogs_findpts_local_3

--- 3rd_party/findpts/ogsFindptsLocal.cpp
+++ 3rd_party/findpts/ogsFindptsLocal.cpp
@@ -9,13 +9,6 @@ extern "C" {
 #include "gslib.h"
 #include "ogsFindpts.h"
 
-#define   AT(T,var,i)   \
-        (T*)(      (char*)var##_base   +(i)*var##_stride   )
-#define  CAT(T,var,i) \
-  (const T*)((const char*)var##_base   +(i)*var##_stride   )
-#define CATD(T,var,i,d) \
-  (const T*)((const char*)var##_base[d]+(i)*var##_stride[d])
-
 static_assert(std::is_same<dfloat, double>::value, "OGS dfloat is not compatible with GSLIB double");
 static_assert(sizeof(dlong) == sizeof(uint), "OGS dlong is not compatible with GSLIB uint");
 
@@ -65,40 +58,10 @@ void ogs_findpts_local_3(    uint   *const  code_base   , const unsigned  code_s
                            d_x_base, d_x_stride,
                        pn, ogs_fd->o_fd_local);
 
-  if( code_stride == sizeof(dlong)) {
-     d_code_base.copyTo( code_base, sizeof(dlong) *pn);
-  } else {
-    dlong*   h_code_base = new dlong [pn];
-     d_code_base.copyTo( h_code_base, sizeof(dlong) *pn);
-    for(dlong i=0;i<pn;++i) *AT(dlong ,  code, i) =  h_code_base[i];
-    delete []  h_code_base;
-  }
-  if(   el_stride == sizeof(dlong)) {
-       d_el_base.copyTo(   el_base,    el_stride*pn);
-  } else {
-    dlong*     h_el_base = new dlong [pn];
-       d_el_base.copyTo(   h_el_base, sizeof(dlong) *pn);
-    for(dlong i=0;i<pn;++i) *AT(dlong ,    el, i) =    h_el_base[i];
-    delete []    h_el_base;
-  }
-  if(    r_stride == sizeof(dfloat)*3) {
-        d_r_base.copyTo(    r_base,     r_stride*pn);
-  } else {
-    dfloat*     h_r_base = new dfloat[pn*3];
-        d_r_base.copyTo(    h_r_base, sizeof(dfloat)*pn*3);
-    for(dlong i=0;i<pn;++i) for(dlong d=0;d<3;++d) {
-        (AT(dfloat, r, i))[d] = h_r_base[i*3 + d];
-    }
-    delete []     h_r_base;
-  }
-  if(dist2_stride == sizeof(dfloat)) {
-    d_dist2_base.copyTo(dist2_base, dist2_stride*pn);
-  } else {
-    dfloat* h_dist2_base = new dfloat[pn];
-    d_dist2_base.copyTo(h_dist2_base, sizeof(dfloat)*pn);
-    for(dlong i=0;i<pn;++i) *AT(dfloat, dist2, i) = h_dist2_base[i];
-    delete [] h_dist2_base;
-  }
+  d_code_base.copyTo(code_base, sizeof(dlong) * pn);
+  d_el_base.copyTo(el_base, el_stride * pn);
+  d_r_base.copyTo(r_base, r_stride * pn);
+  d_dist2_base.copyTo(dist2_base, dist2_stride * pn);
 }
 
 void ogs_findpts_local_eval_internal_3(
