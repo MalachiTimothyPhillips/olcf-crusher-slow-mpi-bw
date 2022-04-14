2adce259651a982e25ddb433145d4acaa6844546
Malachi Phillips <malachi2@illinois.edu>
2022-04-14 11:38:13 -0500

    Add benchmarkAdvsub
    - Add nEXT in diagnostic information
    - Read cubD, cubInterpT in advsub benchmark
    - Add kernel correctness check (printed when verbosity > 1)
    - Use benchmarker to choose kernel in nekRS

--- src/bench/advsub/main.cpp
+++ src/bench/advsub/main.cpp
@@ -11,17 +11,9 @@
 #include "platform.hpp"
 #include "configReader.hpp"
 
-namespace {
+#include "benchmarkAdvsub.hpp"
 
-occa::kernel subcyclingKernel;
-occa::memory o_elementList;
-occa::memory o_cubD;
-occa::memory o_cubInterpT;
-occa::memory o_BdivW;
-occa::memory o_conv;
-occa::memory o_Ud;
-occa::memory o_NU;
-occa::memory o_invLMM;
+namespace {
 
 int Nelements;
 int Np; 
@@ -29,53 +21,6 @@ int cubNp;
 dlong fieldOffset;
 dlong cubatureOffset;
 bool dealias;
-
-double run(int Ntests)
-{
-  platform->device.finish();
-  MPI_Barrier(MPI_COMM_WORLD);
-  const double start = MPI_Wtime();
-
-  const dfloat c0 = 0.1;
-  const dfloat c1 = 0.2;
-  const dfloat c2 = 0.3;
-
-  for(int test = 0; test < Ntests; ++test) {
-    if(!dealias) {
-      subcyclingKernel(Nelements, o_elementList, o_cubD, fieldOffset,
-        0, o_invLMM, o_BdivW, c0, c1, c2, o_conv, o_Ud, o_NU);
-    } else {
-      subcyclingKernel(Nelements, o_elementList, o_cubD, o_cubInterpT, fieldOffset,
-        cubatureOffset, 0, o_invLMM, o_BdivW, c0, c1, c2, o_conv, o_Ud, o_NU);
-    }
-  }
-
-  platform->device.finish();
-  MPI_Barrier(MPI_COMM_WORLD);
-  return (MPI_Wtime() - start) / Ntests;
-} 
-
-void* (*randAlloc)(int);
-
-void* rand32Alloc(int N)
-{
-  float* v = (float*) malloc(N * sizeof(float));
-
-  for(int n = 0; n < N; ++n)
-    v[n] = drand48();
-
-  return v;
-}
-
-void* rand64Alloc(int N)
-{
-  double* v = (double*) malloc(N * sizeof(double));
-
-  for(int n = 0; n < N; ++n)
-    v[n] = drand48();
-
-  return v;
-}
 
 } // namespace
 
@@ -194,111 +139,11 @@ int main(int argc, char** argv)
   platform = platform_t::getInstance(options, MPI_COMM_WORLD, MPI_COMM_WORLD); 
   const int Nthreads =  omp_get_max_threads();
 
-  // build+load kernel
-  occa::properties props = platform->kernelInfo + meshKernelProperties(N);
-  static constexpr int nFields = 3;
-  props["defines/p_cubNq"] = cubNq;
-  props["defines/p_cubNp"] = cubNp;
-  props["defines/p_nEXT"] = nEXT;
-  props["defines/p_NVfields"] = nFields;
-  props["defines/p_MovingMesh"] = 0;
-
-  std::string kernelName;
-  if(dealias){
-    kernelName = "subCycleStrongCubatureVolumeHex3D";
+  if(Ntests != -1){
+    benchmarkAdvsub(Nelements, Nq, cubNq, nEXT, dealias, 2, Ntests, true);
   } else {
-    kernelName = "subCycleStrongVolumeHex3D";
+    benchmarkAdvsub(Nelements, Nq, cubNq, nEXT, dealias, 2, 10.0, true);
   }
-
-  const std::string ext = (platform->device.mode() == "Serial") ? ".c" : ".okl";
-  std::string fileName = 
-    installDir + "/okl/nrs/" + kernelName + ext;
-  
-  // currently lacking a native implementation of the non-dealiased kernel
-  if(!dealias) fileName = installDir + "/okl/nrs/subCycleHex3D.okl";
-
-  subcyclingKernel = platform->device.buildKernel(fileName, props, true);
-
-  // populate arrays
-
-  dlong* elementList = (dlong*) calloc(Nelements, sizeof(dlong));
-  for(int e = 0; e < Nelements; ++e){
-    elementList[e] = e;
-  }
-  o_elementList = platform->device.malloc(Nelements * sizeof(dlong), elementList);
-  free(elementList);
-
-
-  randAlloc = &rand64Alloc; 
-
-  void *invLMM   = randAlloc(Nelements * Np);
-  void *cubD  = randAlloc(cubNq * cubNq);
-  void *NU  = randAlloc(nFields * fieldOffset);
-  void *conv  = randAlloc(nFields * cubatureOffset * nEXT);
-  void *cubInterpT  = randAlloc(Nq * cubNq);
-  void *Ud  = randAlloc(nFields * fieldOffset);
-
-  o_invLMM = platform->device.malloc(Nelements * Np * wordSize, invLMM);
-  free(invLMM);
-  o_cubD = platform->device.malloc(cubNq * cubNq * wordSize, cubD);
-  free(cubD);
-  o_NU = platform->device.malloc(nFields * fieldOffset * wordSize, NU);
-  free(NU);
-  o_conv = platform->device.malloc(nFields * cubatureOffset * nEXT * wordSize, conv);
-  free(conv);
-  o_cubInterpT = platform->device.malloc(Nq * cubNq * wordSize, cubInterpT);
-  free(cubInterpT);
-  o_Ud = platform->device.malloc(nFields * fieldOffset * wordSize, Ud);
-  free(Ud);
-
-  // warm-up
-  double elapsed = run(10);
-  const int elapsedTarget = 10;
-  if(Ntests < 0) Ntests = elapsedTarget/elapsed;
-
-  // ***** 
-  elapsed = run(Ntests);
-  // ***** 
- 
-  // print statistics
-  const dfloat GDOFPerSecond = nFields * (size * Nelements * (N * N * N) / elapsed) / 1.e9;
-
-  size_t bytesPerElem = 2 * nFields * Np; // Ud, NU
-  bytesPerElem += Np; // inv mass matrix
-  bytesPerElem += nFields * cubNp * nEXT; // U(r,s,t)
-
-  size_t otherBytes = cubNq * cubNq; // D
-  if(cubNq > Nq){
-    otherBytes += Nq * cubNq; // interpolator
-  }
-  otherBytes   *= wordSize;
-  bytesPerElem *= wordSize;
-  const double bw = (size * (Nelements * bytesPerElem + otherBytes) / elapsed) / 1.e9;
-
-  double flopCount = 0.0; // per elem basis
-  if(cubNq > Nq){
-    flopCount += 6. * cubNp * nEXT; // extrapolate U(r,s,t) to current time
-    flopCount += 18. * cubNp * cubNq; // apply Dcub
-    flopCount += 9. * Np; // compute NU
-    flopCount += 12. * Nq * (cubNp + cubNq * cubNq * Nq + cubNq * Nq * Nq); // interpolation
-  } else {
-    flopCount = Nq * Nq * Nq * (18. * Nq + 6. * nEXT + 24.);
-  }
-  const double gflops = (size * flopCount * Nelements / elapsed) / 1.e9;
-
-  if(rank == 0)
-    std::cout << "MPItasks=" << size
-              << " OMPthreads=" << Nthreads
-              << " NRepetitions=" << Ntests
-              << " N=" << N
-              << " cubN=" << cubN
-              << " Nelements=" << size * Nelements
-              << " elapsed time=" << elapsed
-              << " wordSize=" << 8*wordSize
-              << " GDOF/s=" << GDOFPerSecond
-              << " GB/s=" << bw
-              << " GFLOPS/s=" << gflops
-              << "\n";
 
   MPI_Finalize();
   exit(0);
