05149a799593ad55aee030fffe884a38cb8309b1
Malachi Phillips <malachi2@illinois.edu>
2022-04-28 13:04:49 -0500

    Wrap common variable name with `{}` to avoid polluting the namespace

--- src/lib/nekrs.cpp
+++ src/lib/nekrs.cpp
@@ -176,10 +176,12 @@ void setup(MPI_Comm commg_in, MPI_Comm comm_in,
 
   nrs = new nrs_t();
 
-  int result = 0;
-  MPI_Comm_compare(commg, comm, &result);
+  {
+    int result = 0;
+    MPI_Comm_compare(commg, comm, &result);
 
-  nrs->multiSession = (result == MPI_UNEQUAL);
+    nrs->multiSession = (result == MPI_UNEQUAL);
+  }
 
   nrsSetup(comm, options, nrs);
 
